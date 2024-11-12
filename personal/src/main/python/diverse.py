2a795c865ea1326fcf25ddfa0d28e954978678de
haakoaal <haakoaal@stud.ntnu.no>
2024-11-12 13:38:27 +0100

    comments

--- personal/src/main/python/diverse.py
+++ personal/src/main/python/diverse.py
@@ -7,13 +7,15 @@ def perfectShuffle(list):
         repeat -= 1
     return newList
 
-
 def defaultParam(check=True):
     print("True" if check else "False")
     
-def stringReplace(string, a, b):
-    '''Replaces instances of a in string with b'''
-    if(isinstance(string, str) and isinstance(a, str) and isinstance(b, str)):
-        print(string.replace(a,b))
-    else:
-        print("Input needs to be string")
+def tupleTest(tuple):
+    variable1=0
+    variable2=1
+    variable1, variable2 = tuple
+    print(f"{variable1}, and {variable2}")
+    
+
+testvariable = (300,"stairs")
+tupleTest(testvariable)
--- personal/src/main/python/randomGroup.py
+++ personal/src/main/python/randomGroup.py
@@ -1,9 +1,11 @@
 import random
 
 def randomgroups(elever,gruppestørrelse):
-    "Oppretter tilfeldige grupper av elevene, basert på ønsket gruppestørrelse"
-    "Param: Liste som skal tilfeldig fordeles, og ønsket gruppestørrelse"
-    "Returns: En liste med tilfeldige grupper i lister."
+    """
+    Oppretter tilfeldige grupper av elevene, basert på ønsket gruppestørrelse\n
+    Param: Liste som skal tilfeldig fordeles, og ønsket gruppestørrelse\n
+    Returns: En liste med tilfeldige grupper i lister.
+    """
     #Validering
     if ( not (isinstance(elever,list))):
         print("Trenger en liste!")
@@ -48,5 +50,14 @@ def finPrint(liste):
     
 testListe = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 randomgroups(testListe,4)
+"""
+Gir output:
+Antall grupper: 3, til overs: 3
+Shuffle: [10, 2, 15, 14, 1, 5, 9, 6, 12, 13, 3, 7, 11, 4, 8]
+Gruppe 1: [10, 2, 15, 14]
+Gruppe 2: [1, 5, 9, 6]
+Gruppe 3: [12, 13, 3, 7]
+Gruppe 4: [11, 4, 8]
+"""
 
       
