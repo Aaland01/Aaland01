01c3ca76545e3dedbccc8a85c443be13534ac276
haakoaal <haakoaal@stud.ntnu.no>
2025-02-26 09:02:04 +1000

    australian qol functions

--- personal/src/main/python/diverse.py
+++ personal/src/main/python/diverse.py
@@ -15,7 +15,44 @@ def tupleTest(tuple):
     variable2=1
     variable1, variable2 = tuple
     print(f"{variable1}, and {variable2}")
-    
 
-testvariable = (300,"stairs")
-tupleTest(testvariable)
+
+def australianToNOK(amount):
+    perMonth = (amount * 52) / 12
+    NOK = perMonth * 7.1
+    print(NOK)
+    
+    
+australianToNOK(575/2)
+
+
+def timeConverterNOK(nokTime):
+    """ Noktime format: nnnn, i.e. 1400 means 2pm"""
+    australianAhead = 9
+    hour = str(nokTime)[:2]
+    mins = str(nokTime)[2:4]
+    print(f"Hour: {hour}")
+    print(f"Minutes: {mins}")
+    australianTime = int(hour) + australianAhead
+    print(f"Time: {australianTime}")
+    if (australianTime - 24 >= 0):
+        australianTime = australianTime - 24
+    print(str(australianTime) + mins)
+    
+timeConverterNOK(1630)
+
+def timeConverterAUS(ausTime):
+    """ Noktime format: nnnn, i.e. 1400 means 2pm"""
+    norBehind = -9
+    hour = str(ausTime)[:2]
+    mins = str(ausTime)[2:4]
+    print(f"Hour: {hour}")
+    print(f"Minutes: {mins}")
+    norTime = int(hour) + norBehind
+    print(f"Time: {norTime}")
+    if (norTime - 24 >= 0):
+        norTime = norTime - 24
+    print(str(norTime) + mins)
+    
+timeConverterAUS(1100)
+    
